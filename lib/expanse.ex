de6f8f32bc6eb830ef3c4b416aff3f6611784b9c
Jason Clark <mithereal@gmail.com>
2017-08-29 21:33:09 -0700

    added documentation

---
+++ lib/expanse.ex
@@ -0,0 +1,12 @@
+defmodule Expanse do
+
+ @moduledoc ~S"""
+  This module has many different Time and Length conversion functions located in the approiate module
+  
+  Expanse.Time
+  
+  Expanse.Length
+  
+  """
+  
+end
